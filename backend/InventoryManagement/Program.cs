420b55a8db38ee9ac89ab6dc8578637aa1c541ef
duchmhe181668 <duchmhe181668@fpt.edu.vn>
2025-09-23 11:03:17 +0700

    frontend sieu dep trai

--- backend/InventoryManagement/Program.cs
+++ backend/InventoryManagement/Program.cs
@@ -8,6 +8,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod());
+});
 
 // K?t n?i SQL Server
 builder.Services.AddDbContext<AppDbContext>(options =>
@@ -23,7 +30,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseCors("AllowAll");
 app.UseAuthorization();
 
 app.MapControllers();
